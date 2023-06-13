08125fd7bbe527b3c1560821193fdf3ea26273d7
n3taway <n3taway@gmail.com>
2023-06-13 16:02:28 +0800

    feat: 使用有道智云appKey翻译句子

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const entities = new Entities();
 const parseString = require('xml2js').parseString;
 const isChinese = require('is-chinese');
 const ora = require('ora');
+const qs = require('querystringify');
+const CryptoJS = require('crypto-js');
+
+function truncate(q) {
+  var len = q.length;
+  if (len <= 20) return q;
+  return q.substring(0, 10) + len + q.substring(len - 10, len);
+}
 
 module.exports = function (word, options, callback) {
   console.log('');
@@ -37,39 +45,62 @@ module.exports = function (word, options, callback) {
 
   // iciba
   isTrueOrUndefined(iciba) &&
-    needle.get(SOURCE.iciba.replace('${word}', endcodedWord), { parse: false }, function (error, response) {
-      if (error) {
-        console.log(chalk.yellow(`访问 iciba 失败，请检查网络`));
-      } else if (response.statusCode == 200) {
-        parseString(response.body, function (err, result) {
-          if (err) {
-            return;
-          }
-          print.iciba(result.dict, options);
-        });
-      }
-      callbackAll();
-    });
-
-  // youdao
-  isTrueOrUndefined(youdao) &&
     needle.get(
-      SOURCE.youdao.replace('${word}', endcodedWord),
+      SOURCE.iciba.replace('${word}', endcodedWord),
       { parse: false },
       function (error, response) {
         if (error) {
-          console.log(chalk.yellow(`访问 youdao 失败，请检查网络`));
+          console.log(chalk.yellow(`访问 iciba 失败，请检查网络`));
         } else if (response.statusCode == 200) {
-          try {
-            const data = JSON.parse(entities.decode(response.body));
-            print.youdao(data, options);
-          } catch (e) {
-            // 来自您key的翻译API请求异常频繁，为保护其他用户的正常访问，只能暂时禁止您目前key的访问
-          }
+          parseString(response.body, function (err, result) {
+            if (err) {
+              return;
+            }
+            print.iciba(result.dict, options);
+          });
         }
         callbackAll();
       },
     );
+
+  const appKey = '';
+  const key = ''; //注意：暴露appSecret，有被盗用造成损失的风险
+  const salt = new Date().getTime();
+  const curtime = Math.round(new Date().getTime() / 1000);
+  const query = word;
+  // 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
+  const from = 'auto';
+  const to = 'auto';
+  const str1 = appKey + truncate(query) + salt + curtime + key;
+  const sign = CryptoJS.SHA256(str1).toString(CryptoJS.enc.Hex);
+
+  const queryData = {
+    q: query,
+    appKey: appKey,
+    salt: salt,
+    from: from,
+    to: to,
+    sign: sign,
+    signType: 'v3',
+    curtime: curtime,
+  };
+  const url = `https://openapi.youdao.com/api?${qs.stringify(queryData)}`;
+
+  // youdao
+  isTrueOrUndefined(youdao) &&
+    needle.get(url, { parse: false }, function (error, response) {
+      if (error) {
+        console.log(chalk.yellow(`访问 youdao 失败，请检查网络`));
+      } else if (response.statusCode == 200) {
+        try {
+          const data = JSON.parse(entities.decode(response.body));
+          print.youdao(data, options);
+        } catch (e) {
+          // 来自您key的翻译API请求异常频繁，为保护其他用户的正常访问，只能暂时禁止您目前key的访问
+        }
+      }
+      callbackAll();
+    });
 
   print.chatgpt(word, options);
 };
